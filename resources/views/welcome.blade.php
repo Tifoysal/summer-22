b8874e6b8f3aaf1269e5160600a6a1b4af03f272
Tifoysal <foysal11203004@gmail.com>
2022-06-08 16:09:31 +0600

    initialize repo

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Kodeeo</title>
+
+        <!-- Fonts -->
+
+    </head>
+    <body class="antialiased">
+
+
+    <h1>Welcome to kodeeo limited</h1>
+    <a class="btn btn-success" href="{{url('/about')}}">Click to show About Us</a>
+
+
+    </body>
+</html>
